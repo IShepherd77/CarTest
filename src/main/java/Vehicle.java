ce2a779f302d9c09a4e0da4f6343c7c5fba27e38
IShepherd77 <ianshepherd77@gmail.com>
2020-01-14 15:39:44 +0000

    set up vehicle class

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -5,18 +5,26 @@ public class Vehicle {
     protected String colour;
     protected String registrationNumber;
     protected double valueOfVehicle;
+    protected String makeOfVehicle;
 
 
-    Vehicle(int numOfWheels, int numOfDoors, String colour, String regNo, double value){
+    Vehicle(int numOfWheels, int numOfDoors, String colour, String regNo, double value, String make){
         this.numberOfWheels = numOfWheels;
         this.numberOfDoors = numOfDoors;
         this.colour = colour;
         this.registrationNumber = regNo;
         this.valueOfVehicle = value;
+        this.makeOfVehicle = make;
     }
 
     public String whichVehicle(){
         return "Vroom, vroom, I am the vehicle";
+    }
+
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle(4, 5, "grey", "SM67 VKS", 40000.00, "BMW");
+
+        System.out.println(car.whichVehicle());
     }
 
 }
