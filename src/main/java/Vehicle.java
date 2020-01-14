8b6cb600fa04a6926a01f71d484e22432aca5429
IShepherd77 <ianshepherd77@gmail.com>
2020-01-14 15:27:00 +0000

    set up vehicle constructor

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -6,4 +6,13 @@ public class Vehicle {
     protected String registrationNumber;
     protected double valueOfVehicle;
 
+
+    Vehicle(int numOfWheels, int numOfDoors, String colour, String regNo, double value){
+        this.numberOfWheels = numOfWheels;
+        this.numberOfDoors = numOfDoors;
+        this.colour = colour;
+        this.registrationNumber = regNo;
+        this.valueOfVehicle = value;
+    }
+
 }
